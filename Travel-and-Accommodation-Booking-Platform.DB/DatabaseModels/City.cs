96bf6dfd8d79273aed6a6e4621798f0a05e621ef
Omar Amer <oiamer4778@gmail.com>
2025-11-27 18:53:40 +0200

    feat: added City database model.

---
+++ Travel-and-Accommodation-Booking-Platform.DB/DatabaseModels/City.cs
@@ -0,0 +1,15 @@
+namespace Travel_and_Accommodation_Booking_Platform.DB.DatabaseModels;
+
+public class City
+{
+    public Guid CityId { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string? ThumbnailUrl { get; set; }
+    public string Country { get; set; } = null!;
+    public string PostOffice { get; set; } = null!;
+    public int NumberOfHotels { get; set; }
+
+
+    public ICollection<Hotel> Hotels { get; set; } = new List<Hotel>();
+}
