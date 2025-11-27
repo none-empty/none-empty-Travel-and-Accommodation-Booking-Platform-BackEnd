27e2fd81fd38db0d5e5325fd467192463e90fc8c
Omar Amer <oiamer4778@gmail.com>
2025-11-27 19:03:47 +0200

    feat: added PaymentMethod enum.

---
+++ Travel-and-Accommodation-Booking-Platform.DB/StateEnums/PaymentMethod.cs
@@ -0,0 +1,15 @@
+namespace Travel_and_Accommodation_Booking_Platform.DB.StateEnums;
+
+public enum PaymentMethod
+{
+    Visa, 
+    MasterCard, 
+    American_Express, 
+    Discover,
+    Apple_Pay, 
+    Google_Pay, 
+    Samsung_Pay,
+    PayPal, 
+    Stripe, 
+    Square
+}
