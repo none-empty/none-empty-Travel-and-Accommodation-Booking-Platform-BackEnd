4885141e777702435cae6a618fd814b0cbfe6f4e
Omar Amer <oiamer4778@gmail.com>
2025-11-27 19:06:38 +0200

    feat: added User database model.

---
+++ Travel-and-Accommodation-Booking-Platform.DB/DatabaseModels/User.cs
@@ -0,0 +1,13 @@
+namespace Travel_and_Accommodation_Booking_Platform.DB.DatabaseModels;
+
+public class User
+{
+    public Guid UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public string Email { get; set; } = null!;
+
+
+    public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+}
