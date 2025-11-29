06411accd027cb12f9e9ea966ce88cdd98d1345f
Omar Amer <oiamer4778@gmail.com>
2025-11-29 15:47:54 +0200

    feat: registered JwtSettings, JwtTokenGenerator and ConfigureJwtBearerOptions to the services container. And also added Authentication.

--- Travel-and-Accommodation-Booking-Platform-BackEnd/Program.cs
+++ Travel-and-Accommodation-Booking-Platform-BackEnd/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Authentication.AccessTokens;
 using Travel_and_Accommodation_Booking_Platform.DB.AppDbContextFiles;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +11,23 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         builder.Configuration.GetSection("connectionStrings")?["sqlserver"])
 );
  
+builder.Services.Configure<JwtSettings>(
+        builder.Configuration.GetSection(JwtSettings.SectionName));
+
+builder.Services.AddSingleton<ITokenGenerator, JwtTokenGenerator>();
+builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer();
+
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
