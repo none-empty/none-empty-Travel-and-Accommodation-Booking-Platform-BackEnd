5d1f4f10a067354f7668f86ae6d096c7316bec64
Omar Amer <oiamer4778@gmail.com>
2025-11-29 19:54:08 +0200

    fix: fixed the validation bug.

--- Travel-and-Accommodation-Booking-Platform-BackEnd/Program.cs
+++ Travel-and-Accommodation-Booking-Platform-BackEnd/Program.cs
@@ -1,11 +1,13 @@
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Travel_and_Accommodation_Booking_Platform_BackEnd.Authentication.AccessTokens;
 using Travel_and_Accommodation_Booking_Platform.DB.AppDbContextFiles;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var config = builder.Configuration;
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetSection("connectionStrings")?["sqlserver"])
@@ -19,7 +21,21 @@ builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtB
 
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer();
+    .AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(config["JwtSettings:SymmetricSecurityKey"]!)),
+
+            ValidIssuer = config["JwtSettings:Issuer"],
+            ValidAudience = config["JwtSettings:Audience"],
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidateIssuer = true,
+            ValidateAudience = true
+        };
+    });
 
 
 builder.Services.AddOpenApi();
@@ -38,9 +54,12 @@ if (app.Environment.IsDevelopment())
  
 app.UseHttpsRedirection();
 
- 
- 
+app.MapGet("/tok",  (ITokenGenerator generator) =>
+{
+    return generator.GenerateToken(Guid.NewGuid(), "user", "fg@g.com", "dumb");
+});
 
+app.MapGet("/hello", () => "you are ok").RequireAuthorization().WithName("xx");
 app.Run();
 
  
