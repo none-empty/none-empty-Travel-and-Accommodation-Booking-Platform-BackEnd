9c338fa66a0d3fc66bc1a5cf452ec411100cb45b
Omar Amer <oiamer4778@gmail.com>
2025-12-06 19:08:34 +0200

    refactor: moved JwtSettings to the Infrastructure layer.

--- Travel-and-Accommodation-Booking-Platform-BackEnd.Infrastructure/Jwt/JwtSettings.cs
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Infrastructure/Jwt/JwtSettings.cs
@@ -1,8 +1,8 @@
-namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Authentication.AccessTokens;
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.Jwt;
 
 public class JwtSettings
 {
-    public const string SectionName = nameof(JwtSettings); // Use a constant for the section name
+    public const string SectionName = nameof(JwtSettings);  
     public string SymmetricSecurityKey { get; set; } = string.Empty;
     public string Issuer { get; set; } = string.Empty;
     public string Audience { get; set; } = string.Empty;
