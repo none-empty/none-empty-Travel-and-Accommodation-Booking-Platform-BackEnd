6fc704128be00f6166a291547402b4e413a36ea8
Omar Amer <oiamer4778@gmail.com>
2025-12-06 19:07:18 +0200

    refactor: moved ITokenGenerator to the Application Layer.

--- Travel-and-Accommodation-Booking-Platform-BackEnd.Application/Common/Interfaces/ITokenGenerator.cs
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Application/Common/Interfaces/ITokenGenerator.cs
@@ -1,4 +1,4 @@
-namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Authentication.AccessTokens;
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Common.Interfaces;
 
 public interface ITokenGenerator
 {
