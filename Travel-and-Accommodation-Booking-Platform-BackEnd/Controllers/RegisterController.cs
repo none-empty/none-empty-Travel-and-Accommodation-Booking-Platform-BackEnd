3e6d663baded4f4786ae5c8880764a99e90f06ae
Omar Amer <oiamer4778@gmail.com>
2025-12-08 18:49:54 +0200

    feat: added the RegisterController with Register method.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Features.Users.Commands.RegisterUser;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.DTOs;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.MapperlyMappings;
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class RegisterController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly UserRegistrationRequestToRegisterUserCommandMapper _mapper;
+    public RegisterController(IMediator mediator,UserRegistrationRequestToRegisterUserCommandMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+     
+    [HttpPost] 
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register([FromBody]UserRegistrationRequest inputData)
+    {
+        var command = _mapper.MapToCommand(inputData);
+        var result = await _mediator.Send(command);
+
+        return StatusCode(StatusCodes.Status201Created, result);
+    }
+}
