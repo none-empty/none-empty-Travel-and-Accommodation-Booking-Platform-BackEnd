32f6c56180b38df4ddf2bc38883af8124ba8058d
Omar Amer <oiamer4778@gmail.com>
2025-12-08 18:44:44 +0200

    feat: added the RegisterUserCommand to the application layer.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Application/Features/Users/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,7 @@
+using MediatR;
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Features.Users.Commands.RegisterUser;
+
+public record RegisterUserCommand(string UserName,string Password,string Email,string PhoneNumber) : IRequest<RegisterUserResponse>;
+
+public record RegisterUserResponse(int UserId, string Email);
