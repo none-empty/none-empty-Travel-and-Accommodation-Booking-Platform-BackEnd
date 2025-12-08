4f72afd4442cd77479a76d4ba33539d113427fb8
Omar Amer <oiamer4778@gmail.com>
2025-12-08 18:40:13 +0200

    feat: added IRepository<T> interface to the Application layer.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Application/Common/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Common.Interfaces.Repositories;
+
+public interface IRepository<T> where T : class   
+{   
+      Task AddAsync(T entity);   
+      Task DeleteAsync(T entity);   
+      Task UpdateAsync(T entity);
+      Task<T>GetByIdAsync(int id);
+}
