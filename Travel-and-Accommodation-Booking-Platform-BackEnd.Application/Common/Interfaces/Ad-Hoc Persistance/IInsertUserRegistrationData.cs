a3337adb28925729fd2cf75b46b703c61c409541
Omar Amer <oiamer4778@gmail.com>
2025-12-09 20:12:46 +0200

    feat: added IInsertUserRegistrationData Ad-Hoc interface.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Application/Common/Interfaces/Ad-Hoc Persistance/IInsertUserRegistrationData.cs
@@ -0,0 +1,9 @@
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Domain.DatabaseModels;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Domain.RefreshTokensFiles;
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Common.Interfaces.Ad_Hoc_Persistance;
+
+public interface IInsertUserRegistrationData
+{
+    Task Execute(User user,RefreshToken token);
+}
