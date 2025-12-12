59d1aa61be6365339242f4b65ee4be9235e53983
Omar Amer <oiamer4778@gmail.com>
2025-12-12 22:20:49 +0200

    feat: added the RefreshTokenRepository to the ioc container.

--- Travel-and-Accommodation-Booking-Platform-BackEnd/ServicesRegistrations/Repositories.cs
+++ Travel-and-Accommodation-Booking-Platform-BackEnd/ServicesRegistrations/Repositories.cs
@@ -1,4 +1,5 @@
 using Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Common.Interfaces.Repositories;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Domain.RefreshTokensFiles;
 using Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.Repositories;
 
 namespace Travel_and_Accommodation_Booking_Platform_BackEnd.ServicesRegistrations;
@@ -8,6 +9,7 @@ public static class Repositories
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRepository<RefreshToken>, RefreshTokenRepository>();
         return services;
     }
 }
