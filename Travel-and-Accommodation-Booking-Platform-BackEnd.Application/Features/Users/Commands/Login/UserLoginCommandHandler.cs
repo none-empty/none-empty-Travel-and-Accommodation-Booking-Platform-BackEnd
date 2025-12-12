50a559ea682def5eb545431637caa2a2b402f210
Omar Amer <oiamer4778@gmail.com>
2025-12-12 22:21:40 +0200

    feat: added UserLoginCommandHandler to the application layer.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Application/Features/Users/Commands/Login/UserLoginCommandHandler.cs
@@ -0,0 +1,47 @@
+using System.Security.Authentication;
+using MediatR;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Common.Interfaces;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Common.Interfaces.Repositories;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Domain.DatabaseModels;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Domain.RefreshTokensFiles;
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Features.Users.Commands.Login;
+
+public class UserLoginCommandHandler : IRequestHandler<UserLoginCommand, UserLoginResponse>
+{
+    private readonly IRepository<RefreshToken> _refreshTokenRepo;
+    private readonly IUserRepository _userRepo;
+    private readonly ITokenGenerator _tokenGenerator;
+    private readonly IPasswordHasher _hasher;
+    public UserLoginCommandHandler(IRepository<RefreshToken>refreshTokenRepo,
+        IUserRepository userRepo,ITokenGenerator tokenGenerator,IPasswordHasher hasher)
+    {
+        _refreshTokenRepo = refreshTokenRepo;
+        _userRepo = userRepo;
+        _tokenGenerator = tokenGenerator;
+        _hasher = hasher;
+    }
+
+    public async Task<UserLoginResponse> Handle(UserLoginCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepo.GetBy(user => user.Email.Equals(request.Email));
+
+        if (InvalidCredentials(user,request.Password)) throw new InvalidCredentialException("invalid credentials");
+        
+        var accessToken = _tokenGenerator.GenerateToken(
+            user.UserId,
+            "user", //TODO change
+            user.Email,
+            user.UserName
+        );
+
+        var refreshToken = _tokenGenerator.GenerateRefreshToken(user.UserId);
+        await _refreshTokenRepo.AddAsync(refreshToken);
+        return new UserLoginResponse(accessToken, refreshToken.Token);
+    }
+
+    private bool InvalidCredentials(User? user,string password)
+    {
+        return user is null || !_hasher.VerifyPassword(password,user.PasswordHash);
+    }
+}
