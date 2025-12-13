213ddbfc0d093301fc6c48f9b10dab3ac069c7b0
Omar Amer <oiamer4778@gmail.com>
2025-12-13 23:21:20 +0200

    feat: generated the HotelsImagesTable migration files.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Infrastructure/Migrations/20251213211526_HotelsImagesTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotelsImagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HotelImage",
+                columns: table => new
+                {
+                    ImageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HotelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelImage", x => x.ImageId);
+                    table.ForeignKey(
+                        name: "FK_HotelImage_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "HotelId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelImage_HotelId",
+                table: "HotelImage",
+                column: "HotelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelImage");
+        }
+    }
+}
