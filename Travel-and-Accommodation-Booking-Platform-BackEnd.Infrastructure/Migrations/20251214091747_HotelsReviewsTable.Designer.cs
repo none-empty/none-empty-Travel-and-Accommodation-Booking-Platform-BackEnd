2900c3cbf6e08f9d88822c524b0df47ad93a67a9
Omar Amer <oiamer4778@gmail.com>
2025-12-14 11:22:19 +0200

    feat: generated HotelsReviews table migration files.

--- Travel-and-Accommodation-Booking-Platform-BackEnd.Infrastructure/Migrations/20251214091747_HotelsReviewsTable.Designer.cs
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Infrastructure/Migrations/20251214091747_HotelsReviewsTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.AppDbContextFiles;
 
@@ -11,9 +12,11 @@ using Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.AppDbCont
 namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251214091747_HotelsReviewsTable")]
+    partial class HotelsReviewsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Infrastructure/Migrations/20251214091747_HotelsReviewsTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotelsReviewsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HotelReview",
+                columns: table => new
+                {
+                    ReviewId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HotelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Review = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelReview", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_HotelReview_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "HotelId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HotelReview_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelReview_HotelId",
+                table: "HotelReview",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelReview_UserId",
+                table: "HotelReview",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelReview");
+        }
+    }
+}
