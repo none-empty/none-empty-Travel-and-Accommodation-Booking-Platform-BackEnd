8ac816eabb0eb16b5eb1eefcc5cc7bdf699f455e
Omar Amer <oiamer4778@gmail.com>
2025-12-15 13:15:48 +0200

    feat: added migration files for the UsersRoles Table.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Infrastructure/Migrations/20251215111108_UsersRolesTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersRolesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersRoles", x => new { x.UserId, x.Role });
+                    table.ForeignKey(
+                        name: "FK_UsersRoles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersRoles");
+        }
+    }
+}
