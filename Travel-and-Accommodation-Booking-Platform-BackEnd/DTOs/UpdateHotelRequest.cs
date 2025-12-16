71f1c9f91a40d88e239ab16bce65fcf80ac09d17
Omar Amer <oiamer4778@gmail.com>
2025-12-16 17:58:39 +0200

    feat: added AdminHotelEndpoints nessessary requests DTOs to the web app.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd/DTOs/UpdateHotelRequest.cs
@@ -0,0 +1,7 @@
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Domain.StateEnums;
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.DTOs;
+
+public record UpdateHotelRequest(Guid HotelId,string HotelName, Guid CityId, int StarRating, string Owner,
+    string? ThumbnailUrl, HotelCategory Category ,decimal PricePerNight, string? Description 
+    ,string Address, string PhoneNumber);
