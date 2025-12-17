6253c7876c6453beba805a00d5ebf6a1a482cd1c
Omar Amer <oiamer4778@gmail.com>
2025-12-17 13:56:00 +0200

    feat: added FetchNextRecordsQueryHandler<T3Entity> to the application layer.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Application/Features/Search/FetchNextRecordsQueryHandler.cs
@@ -0,0 +1,30 @@
+using MediatR;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Common.Interfaces.Ad_Hoc_Persistance;
+using Travel_and_Accommodation_Booking_Platform_BackEnd.Application.SearchPredicatesRepos;
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Application.Features.Search;
+
+public class FetchNextRecordsQueryHandler<T3Entity> : IRequestHandler<FetchNextRecordsQuery<T3Entity>,
+    FetchNextRecordsQueryResponse<T3Entity>> where T3Entity : class
+{
+    private readonly ISearchPredicateRepo<T3Entity> _searchPredicateRepo;
+    private readonly IFetchNextRecordsFromDatabase<T3Entity> _fetchNextRecordsFromDatabase;
+
+    public FetchNextRecordsQueryHandler(ISearchPredicateRepo<T3Entity> searchPredicateRepo
+    ,IFetchNextRecordsFromDatabase<T3Entity> fetchNextRecordsFromDatabase)
+    {
+        _searchPredicateRepo = searchPredicateRepo;
+        _fetchNextRecordsFromDatabase = fetchNextRecordsFromDatabase;
+    }
+
+    public Task<FetchNextRecordsQueryResponse<T3Entity>> Handle(FetchNextRecordsQuery<T3Entity> request, CancellationToken cancellationToken)
+    {
+        var searchPredicates = _searchPredicateRepo.GetSearchPredicates(request.SearchTopic,request.PrevElementId);
+        var getPrimaryKey = _searchPredicateRepo.GetPrimaryKeyDelegate();
+
+        return _fetchNextRecordsFromDatabase.Execute(searchPredicates, getPrimaryKey,
+            request.Limit, request.Down);
+    }
+}
+
+ 
