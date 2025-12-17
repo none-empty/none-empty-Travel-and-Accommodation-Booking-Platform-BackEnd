28ccbe08d61e04f12d0a6778f7d62a31618afddb
Omar Amer <oiamer4778@gmail.com>
2025-12-17 13:52:40 +0200

    feat: added ISearchPredicateRepo<T> to the application layer.

---
+++ Travel-and-Accommodation-Booking-Platform-BackEnd.Application/SearchPredicatesRepos/ISearchPredicateRepo.cs
@@ -0,0 +1,9 @@
+using System.Linq.Expressions;
+
+namespace Travel_and_Accommodation_Booking_Platform_BackEnd.Application.SearchPredicatesRepos;
+
+public interface ISearchPredicateRepo<T>
+{
+    public List<Expression<Func<T, bool>>> GetSearchPredicates(string search,Guid prevElementId);
+    public  Expression<Func<T,Guid>>GetPrimaryKeyDelegate();
+}
